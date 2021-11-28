61a0c21dba5b729ed6c2dfe14fed1cffc6f78d78
your name here <mmakhambetov@yandex.ru>
2021-11-28 22:30:42 +0400

    Create GalleryNavigation component

---
+++ src/components/GalleryNavigation/index.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+import './GalleryNavigation.css';
+
+const GalleryNavigation = ({ galleries }) => {
+  console.log(galleries)
+  return (
+    <nav>
+      <h1>Galleries</h1>
+      <NavLink to='/'>Home</NavLink>
+      {galleries.map(gallery => 
+        <NavLink key={gallery.id} to={`/galleries/${gallery.id}`}>{gallery.name}</NavLink>
+      )}
+    </nav>
+  )
+};
+
+export default GalleryNavigation;
